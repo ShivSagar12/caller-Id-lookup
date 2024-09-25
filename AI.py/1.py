2f67fe6372102756f8ba699e35f8350de6ae6682
Shiv Sagar Singh <163329088+ShivSagar12@users.noreply.github.com>
2024-09-25 15:18:18 +0530

    Delete AI.py directory

--- AI.py/1.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-import os 
-  
-def say(text):
-    os.system(f"say {text}")
-
-if __name__=='__main__' :
-    print("python")
-    say("hellow ,good morning shiv sagar")  
